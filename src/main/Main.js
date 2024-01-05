8a31d888c2f6c274cd5dbf50f3e75bb6c27ac116
이윤성 <chance07236@naver.com>
2024-01-05 13:38:53 +0900

    feat: API 호출방식 수정

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,14 +1,19 @@
+import { useEffect, useState } from 'react';
 import MainSearch from "./MainSearch";
-import MainFile from "./MainFile";
+import MainFiles from "./MainFiles";
+import {getFolders} from '../api'
 import './main.css';
 
-
 function Main() {
-  const handleClick = function(site){
-    const link = site;
-    window.open(link, '_blank');
+  const [folders, setFolders] = useState([]);
+  const getInfo = async () =>{
+    const { folder } = await getFolders();
+    setFolders(folder.links);
   }
-  
+  useEffect(()=>{
+    getInfo();
+  },[]);
+
   return (
     <div className="Main">
       <div className="box">
@@ -16,15 +21,7 @@ function Main() {
           <MainSearch />
         </div>
         <div className="item02">
-          <MainFile value="image1" onClick={handleClick} i={0}/>
-          <MainFile value="image2" onClick={handleClick} i={1}/>
-          <MainFile value="image3" onClick={handleClick} i={2}/>
-          <MainFile value="image4" onClick={handleClick} i={3}/>
-          <MainFile value="image5" onClick={handleClick} i={4}/>
-          <MainFile value="image6" onClick={handleClick} i={5}/>
-          <MainFile value="image7" onClick={handleClick} i={6}/>
-          <MainFile value="image8" onClick={handleClick} i={7}/>
-          <MainFile value="image9" onClick={handleClick} i={8}/>
+          <MainFiles folders={folders}/>
         </div>
       </div>
     </div>
