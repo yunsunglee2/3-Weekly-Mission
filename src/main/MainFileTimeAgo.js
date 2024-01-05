92394fa01a45b563a0942dc83a9f409d2a2d72d9
이윤성 <chance07236@naver.com>
2024-01-05 13:36:20 +0900

    feat: 수정시간 변경 함수 수정

--- src/main/MainFileTimeAgo.js
+++ src/main/MainFileTimeAgo.js
@@ -2,30 +2,36 @@ function TimeDiff(time) {
   const today = new Date();
   const dayAgo = new Date(time);
   const dayDiff = today - dayAgo;
-  const oneMin = 1000*60;
-  const remainingMins = Math.floor(dayDiff / oneMin);
+  const SEC = 1000;
+  const MIN = SEC*60;
+  const HOUR = MIN*60;
+  const DAY = HOUR*24;
+  const MONTH = DAY*30;
+  const YEAR = MONTH*12;
+
+  const remainingSec = Math.floor(dayDiff / SEC);
+  const remainingMin = Math.floor(dayDiff / MIN);
+  const remainingHour = Math.floor(dayDiff / HOUR);
+  const remainingDay = Math.floor(dayDiff / DAY);
+  const remainingMonth = Math.floor(dayDiff / MONTH);
+  const remainingYear = Math.floor(dayDiff / YEAR);
   
-  if (remainingMins < 2){
-    return `${remainingMins}min Ago`
-  } else if (2 < remainingMins && remainingMins < 60){
-    return `${remainingMins}mins Ago`
-  } else if (60 < remainingMins && remainingMins < 60*2) {
-    return `${Math.floor(remainingMins/60)}hour Ago`
-  } else if (60*2 <= remainingMins && remainingMins < 60*24) {
-    return `${Math.floor(remainingMins/60)}hours Ago`
-  } else if (60*24 <= remainingMins && remainingMins < 60*24*2) {
-    return `${Math.floor(remainingMins/60/24)}day Ago`
-  } else if (60*24*2 <= remainingMins && remainingMins < 60*24*30) {
-    return `${Math.floor(remainingMins/60/24)}days Ago`
-  } else if (60*24*30 <= remainingMins && remainingMins < 60*24*30*2) {
-    return `${Math.floor(remainingMins/60/24/30)}month Ago`
-  } else if (60*24*30*2 <= remainingMins && remainingMins < 60*24*30*12) {
-    return `${Math.floor(remainingMins/60/24/30)}months Ago`
-  } else if (60*24*30*12 === remainingMins) {
-    return `${Math.floor(remainingMins/60/24/30/12)}year Ago`
-  } else if (60*24*30*12 < remainingMins) {
-    return `${Math.floor(remainingMins/60/24/30/12)}years Ago`
-  }
+  if (dayDiff < MIN){
+    return `${Math.floor(remainingSec)}sec${ dayDiff < (SEC * 2) ? '' : 's' } Ago`
+  } 
+  if (dayDiff < HOUR){
+    return `${Math.floor(remainingMin)}min${ dayDiff < (MIN * 2) ? '' : 's' } Ago`
+  } 
+  if (dayDiff < DAY) {
+    return `${Math.floor(remainingHour)}hour${ dayDiff < (HOUR * 2) ? '' : 's' } Ago`
+  } 
+  if (dayDiff < MONTH) {
+    return `${Math.floor(remainingDay)}day${ dayDiff < (DAY * 2) ? '' : 's' } Ago`
+  } 
+  if (dayDiff < YEAR) {
+    return `${Math.floor(remainingMonth)}month${ dayDiff < (MONTH * 2) ? '' : 's' } Ago`
+  } 
+    return `${Math.floor(remainingYear)}year${ dayDiff < (YEAR * 2) ? '' : 's' } Ago`
 }
 
 function TimeAgo({time}){
