cebb5f39f09c90b2dd960815e0d05392e46636bf
이윤성 <chance07236@naver.com>
2024-01-14 17:12:13 +0900

    feat: 반환값 상수 처리 수정

--- src/components/main/api/folderApi.js
+++ src/components/main/api/folderApi.js
@@ -2,6 +2,5 @@ const API_BASE_URL_FOLDER = 'https://bootcamp-api.codeit.kr/api/users/1/folders'
 
 export async function getMyFolders() {
   const response = await fetch(API_BASE_URL_FOLDER)
-  const alreadyFolders = await response.json();
-  return alreadyFolders;
+  return await response.json();
 }
--- src/components/main/api/linksApi.js
+++ src/components/main/api/linksApi.js
@@ -1,6 +1,5 @@
 export async function getLinks(id) {
   const API_BASE_URL_LINKS = `https://bootcamp-api.codeit.kr/api/users/1/links${ id ? `?folderId=${id}` : " "}`
   const response = await fetch(API_BASE_URL_LINKS)
-  const folders = await response.json();
-  return folders;
+  return await response.json();
 }
